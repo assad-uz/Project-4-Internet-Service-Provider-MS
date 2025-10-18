3072a2b409550c685e731062660065e99fd7e539
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-10-18 09:47:08 +0600

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', [AdminController::class, 'dashboard']);
+
+// Users
+Route::resource('users', UserController::class);
 
