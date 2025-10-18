7afd03051e2e93d57be9fd0c730875071db6f64a
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-10-18 12:53:40 +0600

    Update

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -67,11 +67,10 @@
           <ul class="menu-inner py-1">
             <!-- Dashboards -->
             <li class="menu-item active open">
-              <a href="javascript:void(0);" class="menu-link">
+              <a href="/dashboard" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-smile"></i>
                 <div class="text-truncate" data-i18n="Dashboards">Dashboards</div>
               </a>
-              
             </li>
 
             <!-- Layouts -->
