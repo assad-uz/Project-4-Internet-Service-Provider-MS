30c2d52ada5b7b5b5052e6f3f6b3b8239678b8de
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-11-01 11:03:27 +0600

    Update

---
+++ app/Http/Controllers/CustomerTypeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomerType;
+use Illuminate\Http\Request;
+
+class CustomerTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomerType $customerType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomerType $customerType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CustomerType $customerType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomerType $customerType)
+    {
+        //
+    }
+}
